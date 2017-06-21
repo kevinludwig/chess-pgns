faf88cc4633dcfb59b971b89f4827645b257944a
Kevin Ludwig <kevin.ludwig@gmail.com>
2017-06-21 06:25:51 -0700

    fix a few errors

--- parse-pgn.js
+++ parse-pgn.js
@@ -2,4 +2,17 @@ const fs = require('fs'),
       pegjs = require('pegjs');
 
 const parser = new pegjs.generate(fs.readFileSync('./grammar.peg', 'utf-8'));
-console.log(parser.parse('[Result "1-0"]\n[White "kevinl"]\n1. {first comment} e4 {second comment} e5 {third comment} 2. d4 exd4 3. Qxd4 Nc6 4. Qe3 Nf6'));
+const pgn = '[Result "1-0"]\n' +
+    '[White "kevinl"]\n' +
+    '1. {first comment} e4 {second comment} e5\n' +
+    '2. d4 exd4+ 3. Nf3+ Nc6 4. Bc4 Bc5 \n' +
+    '5. d1=Q+ exf8=N 6. O-O+ Bxc3 {last comment}\n' +
+    '7. Ra2\n1-0';
+
+/* 
+ * last move half move is broken
+ * no RAV support yet
+ * no NAG support yet
+ */
+const result = parser.parse(pgn);
+console.log(result);
