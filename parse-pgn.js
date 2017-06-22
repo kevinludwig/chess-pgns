74bdc2a40e06d08056c20bbc1c0b89e317a10f08
Kevin Ludwig <kevin.j.ludwig.-nd@disney.com>
2017-06-21 17:01:09 -0700

    add nag support

--- parse-pgn.js
+++ parse-pgn.js
@@ -4,15 +4,14 @@ const fs = require('fs'),
 const parser = new pegjs.generate(fs.readFileSync('./grammar.peg', 'utf-8'));
 const pgn = '[Result "1-0"]\n' +
     '[White "kevinl"]\n' +
-    '1. {first comment} e4 {second comment} e5\n' +
-    '2. d4 exd4+ 3. Nf3+ Nc6 4. Bc4 Bc5 \n' +
-    '5. d1=Q+ exf8=N 6. O-O+ Bxc3 {last comment}\n' +
-    '7. Ra2\n1-0';
+    '1. {first comment} e4 $2 $16 {second comment} e5\n' +
+    '2. d4 exd4+ 3. Nf3+ Nc6 $1 4. Bc4 Bc5 \n' +
+    '5. d1=Q+ exf8=N 6. O-O+ $17 Bxc3 {last comment}\n' +
+    '7. Ra2 Kd7\n1-0';
 
 /* 
  * last move half move is broken
  * no RAV support yet
- * no NAG support yet
  */
 const result = parser.parse(pgn);
-console.log(result);
+console.log(JSON.stringify(result, null, 2));
